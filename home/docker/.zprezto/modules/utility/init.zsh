d55fe74b2f21017d5ea87f5cff61efcceb7dd97b
Widi Harsojo <wharsojo@gmail.com>
2015-06-12 03:04:14 +0800

    update and patch configs

--- home/docker/.zprezto/modules/utility/init.zsh
+++ home/docker/.zprezto/modules/utility/init.zsh
@@ -60,35 +60,35 @@ alias rm="${aliases[rm]:-rm} -i"
 alias type='type -a'
 
 # ls
-if is-callable 'dircolors'; then
-  # GNU Core Utilities
-  alias ls='ls --group-directories-first'
+# if is-callable 'dircolors'; then
+#   # GNU Core Utilities
+#   alias ls='ls --group-directories-first'
 
-  if zstyle -t ':prezto:module:utility:ls' color; then
-    if [[ -s "$HOME/.dir_colors" ]]; then
-      eval "$(dircolors --sh "$HOME/.dir_colors")"
-    else
-      eval "$(dircolors --sh)"
-    fi
+#   if zstyle -t ':prezto:module:utility:ls' color; then
+#     if [[ -s "$HOME/.dir_colors" ]]; then
+#       eval "$(dircolors --sh "$HOME/.dir_colors")"
+#     else
+#       eval "$(dircolors --sh)"
+#     fi
 
-    alias ls="${aliases[ls]:-ls} --color=auto"
-  else
-    alias ls="${aliases[ls]:-ls} -F"
-  fi
-else
-  # BSD Core Utilities
-  if zstyle -t ':prezto:module:utility:ls' color; then
-    # Define colors for BSD ls.
-    export LSCOLORS='exfxcxdxbxGxDxabagacad'
+#     alias ls="${aliases[ls]:-ls} --color=auto" bug on boot2docker
+#   else
+#     alias ls="${aliases[ls]:-ls} -F"
+#   fi
+# else
+#   # BSD Core Utilities
+#   if zstyle -t ':prezto:module:utility:ls' color; then
+#     # Define colors for BSD ls.
+#     export LSCOLORS='exfxcxdxbxGxDxabagacad'
 
-    # Define colors for the completion system.
-    export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
+#     # Define colors for the completion system.
+#     export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
 
-    alias ls="${aliases[ls]:-ls} -G"
-  else
-    alias ls="${aliases[ls]:-ls} -F"
-  fi
-fi
+#     alias ls="${aliases[ls]:-ls} -G"
+#   else
+#     alias ls="${aliases[ls]:-ls} -F"
+#   fi
+# fi
 
 alias l='ls -1A'         # Lists in one column, hidden files.
 alias ll='ls -lh'        # Lists human readable sizes.
