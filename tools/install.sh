543032611ee16cf7c45bb074314e8b44a37bf8d1
Widi Harsojo <wharsojo@gmail.com>
2015-06-12 09:35:36 +0800

    remove unneede mesg

--- tools/install.sh
+++ tools/install.sh
@@ -30,9 +30,7 @@ echo "$MYBOOT2DOCKER/bootlocal.sh" >>$BOOTLOCAL
 chmod +x $BOOTLOCAL
 
 $MYBOOT2DOCKER/tools/ascii-art.sh
-echo "${C2}installed...!$C0"
+echo -e "${C2}installed...!$C0"
 
 $MYBOOT2DOCKER/tools/version.sh
 echo -e "$C0"
-
-$MYBOOT2DOCKER/bootlocal.sh
